43fe4ed19ab4ce252fa7f3e9394e32d1bd3d1758
Rex Chung <rex@rorcraft.com>
2013-04-07 00:11:30 -0700

    update fibers to use bundler

--- fibers.rb
+++ fibers.rb
@@ -1,17 +1,16 @@
-require 'rubygems'
-require 'bundler/setup'
+require 'bundler'
+Bundler.require(:default, :evented)
+
 require 'benchmark'
 
-require 'em-synchrony'
-require 'mysql2'
 require 'em-synchrony/fiber_iterator'
 require 'em-synchrony/activerecord'
 
-require 'pry'
+adapter = RUBY_PLATFORM =~ /java/ ? 'mysql' : 'mysql2'
 
 puts "em-synchrony/activerecord"
 
-ActiveRecord::Base.establish_connection(adapter: "em_mysql2", host: "localhost", username: "root", pool: 10)
+ActiveRecord::Base.establish_connection(adapter: adapter, host: "localhost", username: "root", pool: 10)
 Benchmark.bm do |b|
   b.report("1 select") do
     result = []
@@ -22,21 +21,21 @@ Benchmark.bm do |b|
     # puts result
   end
 
-  b.report("5 Fibers db") do
-    fibers = []
-    result = []
-    EM.synchrony do
-      # EM::Synchrony::Iterator.new(1..5).each do
-      5.times do
-        result << Fiber.new do
-          ActiveRecord::Base.connection.execute("SELECT sleep(3)")
-          # sleep(3)
-        end.resume
-      end
-      EM.stop
-    end
-    puts result
-  end
+  # b.report("5 Fibers db") do
+  #   fibers = []
+  #   result = []
+  #   EM.synchrony do
+  #     # EM::Synchrony::Iterator.new(1..5).each do
+  #     5.times do
+  #       result << Fiber.new do
+  #         ActiveRecord::Base.connection.execute("SELECT sleep(3)")
+  #         # sleep(3)
+  #       end.resume
+  #     end
+  #     EM.stop
+  #   end
+  #   puts result
+  # end
 
   b.report("5 concurrent") do
     result = []
--- threads.rb
+++ threads.rb
@@ -1,6 +1,4 @@
-# require 'rubygems'
 require 'bundler'
-
 Bundler.require(:default)
 
 # puts "mysql2"
