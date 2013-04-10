c4a0840ec83e9c68ee5c16a1f392674dd0d2d406
Rex Chung <rex@rorcraft.com>
2013-04-09 19:22:06 -0700

    add fibonacci for the sake of it

--- nodejs.js
+++ nodejs.js
@@ -15,7 +15,9 @@ var measure = function(name, func, process_fiber)  {
     func();
     var elapsed_time = new Date - start;
     console.log(name + ": " + elapsed_time + "ms")
-    process_fiber.run();
+    if (process_fiber) {
+      process_fiber.run();
+    }
   }).run();
 };
 
@@ -52,12 +54,31 @@ function async_sleepers() {
   Fiber.yield();
 }
 
+function fib(n) {
+  n < 2 ? n : fib(n - 1) + fib(n - 2);
+}
+
+function fib32() {
+  fib(32);
+}
+
+function five_fib32() {
+  var times = 5;
+  while(times--) {
+    fib(32);
+  }
+}
+
 Fiber(function() {
   var process_fiber = Fiber.current;
   measure("sleep(3)", sleeper, process_fiber);
   Fiber.yield();
   measure("5 concurrent", async_sleepers, process_fiber);
   Fiber.yield();
+  measure("fib(32)", fib32, null);
+
+  measure("5 fib(32)", five_fib32, null);
+
   process.exit();
 }).run();
 
