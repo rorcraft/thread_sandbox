0e3d6e0dbd01c521d25fe0e41873df1b5d01c1da
Rex Chung <rex@rorcraft.com>
2013-04-09 19:13:09 -0700

    fix up nodejs version

--- nodejs.js
+++ nodejs.js
@@ -3,27 +3,30 @@ var Fiber = require('fibers');
 var Future = require('fibers/future')
 
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
+
+var pool  = mysql.createPool({
   host     : 'localhost',
   user     : 'root'
 });
 
-connection.connect();
-
-var measure = function(name, func)  {
+var measure = function(name, func, process_fiber)  {
   Fiber(function() {
     var start = new Date;
     func();
     var elapsed_time = new Date - start;
     console.log(name + ": " + elapsed_time + "ms")
+    process_fiber.run();
   }).run();
 };
 
 function sleeper() {
   var fiber = Fiber.current;
-  connection.query('SELECT sleep(3)', function(err, rows, fields) {
-    if (err) throw err;
-    fiber.run();
+  pool.getConnection(function(err, connection) {
+    connection.query('SELECT sleep(3)', function(err, rows, fields) {
+      if (err) throw err;
+      fiber.run();
+      connection.end();
+    });
   });
   Fiber.yield();
 }
@@ -32,21 +35,29 @@ function async_sleepers() {
   var fiber = Fiber.current;
   var ready_count = 0
   var concurrent  = 5
-  var ready = function(err, rows, fields) {
-    if (err) throw err;
-    ready_count++;
-    if(ready_count == 5) {
-      fiber.run();
-    }
-  }
+
   while(concurrent--) {
-    connection.query('SELECT sleep(3)', ready);
+    pool.getConnection(function(err, connection) {
+      var ready = function(err, rows, fields) {
+        if (err) throw err;
+        ready_count++;
+        connection.end();
+        if(ready_count == 5) {
+          fiber.run();
+        }
+      }
+      connection.query('SELECT sleep(3)', ready);
+    });
   }
-  console.log("after loop")
   Fiber.yield();
 }
 
-measure("sleep(3)", sleeper);
-measure("5 concurrent", async_sleepers);
+Fiber(function() {
+  var process_fiber = Fiber.current;
+  measure("sleep(3)", sleeper, process_fiber);
+  Fiber.yield();
+  measure("5 concurrent", async_sleepers, process_fiber);
+  Fiber.yield();
+  process.exit();
+}).run();
 
-connection.end();
